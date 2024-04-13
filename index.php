4d540b94e2c665db76351e4cb3eac78158757ad3
MusicMaestro88 <bastiansitanggang09102005@gmail.com>
2024-04-13 18:21:08 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'App/init.php';
+
+
+$produk1 = new Komik("Naruto", "Masahi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sonny Computer", 25000, 50);
+
+$produk1 = new Komik("Naruto", "Masahi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sonny Computer", 25000, 50);
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk( $produk1 );
+$cetakProduk->tambahProduk( $produk2 );
+echo $cetakProduk->cetak();
