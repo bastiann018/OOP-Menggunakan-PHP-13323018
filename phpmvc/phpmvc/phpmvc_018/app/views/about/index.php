3573744ffc7c2ffb4413fcbea454b92cff6494d0
MusicMaestro88 <bastiansitanggang09102005@gmail.com>
2024-04-13 18:23:04 +0700

    Create index.php

---
+++ phpmvc/phpmvc/phpmvc_018/app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/profile.jpg" alt="Bastian Sitanggang" width="180" class="rounded-circle shadow">
+    <p>Hai nama saya <?= $data['nama']; ?> pekerjaan saya adalah <?= $data['pekerjaan']; ?> umur saya <?= $data['umur']; ?></p>
+</div>
